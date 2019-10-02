1b77f5cd135fc8103081f25ed8723e2449afbda7
sk <skbro10@gmail.com>
2019-10-02 23:49:44 +0545

    tried assostions with meeting room and schedules but currently its not working as planned

--- app/models/meeting_room.rb
+++ app/models/meeting_room.rb
@@ -1,4 +1,5 @@
 class MeetingRoom < ApplicationRecord
 
     belongs_to :user
+    # has_many :schedules, dependent: :destroy 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :meeting_rooms, dependent: :destroy       
+has_many :meeting_rooms, dependent: :destroy       
 end
